faf438e3dfce460eb165995f14f286a4f905a83e
Avi <AviFix@gmail.com>
2020-06-16 21:20:19 +0300

    #1 fix bug #1 Hard coded server url override url and port with the ones written in the plugin config

--- src/http.js
+++ src/http.js
@@ -3,7 +3,7 @@ let request = require("request");
 let server = "http://10.0.0.100/content/";
 
 function setServerData(baseUrl, port) {
-  server = `${baseUrl || 'http://10.0.0.100'}:${port || 80}/content/`;
+  server = `http://${baseUrl || '10.0.0.100'}:${port || 80}/content/`;
 }
 
 function http(url) {
